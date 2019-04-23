3b71193cbc7eaa7b83955e38466037f4e36e02de
bigbluey <joseph.yon@gmail.com>
2019-04-23 15:18:12 -0700

    Added HTML & Javascriot Files

---
+++ assets/js/config.js
@@ -0,0 +1,3 @@
+// API Key
+
+const API_KEY = "pk.eyJ1IjoiYmlnYmx1ZXkiLCJhIjoiY2p1dTVzeHZ3MDBxZTN5cHMwcGdwcTFmdyJ9.FMNXMqklO747udRE11dCwg";
---
+++ assets/js/logic.js
@@ -0,0 +1,26 @@
+// Visualizing-Data-with-Leaflet - logic.js
+
+var earthquakesAPI = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php"
+var platesAPI = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
+// Define Variables for Tile Layers
+var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.satellite",
+  accessToken: API_KEY
+});
+
+var grayscale = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.grayscale",
+  accessToken: API_KEY
+});
+
+var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.outdoors",
+  accessToken: API_KEY
+});
